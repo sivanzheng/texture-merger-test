31f9015bb3b5b4c5cc3a6830caba63f6ea77ab9a
郑晰文 <zhengxw1@pingpongx.com>
2021-10-21 17:33:09 +0800

    update build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.ts',
   output: {
-    publicPath: '/',
+    publicPath: '/dist/',
     filename: 'index.js',
     path: path.resolve(__dirname, './dist'),
   },
